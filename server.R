66d50faf59fbf6170e3be8617e4754f95fe64caa
Barry Polley <barryp@well.com>
2014-11-23 20:25:46 +1300

    Plot works but is not reactive yet

--- server.R
+++ server.R
@@ -12,10 +12,23 @@ shinyServer(function(input, output) {
          type="n",
          xlab="Month during year 2014",
          ylab="Cumulative count of viable Silver Beech seeds")
+    ## setup
+    sites <- c("Borer Flat", "Hunters Hill", "Rough Creek", "Station Creek", "Thisbe Stream", "Turret Head", "Wainui Stream")
+    elementCount <- length(sites)
+    colors <- rainbow(elementCount)
+    linetype <- c(1:elementCount)
+    plotchar <- seq(18, 18+elementCount, 1)
+    ## lines
+    for (i in 1:elementCount) 
+    {
+      loc <- subset(silver, MonitoringPlace == sites[i])
+      lines(loc$month, loc$Cum.Sum,
+            type="b", lwd=2,
+            lty=linetype[i],col=colors[i],pch=plotchar[i])
+    }
+    ## legend
+    legend("topleft", inset=.05, title="[Site]", col=colors, pch=plotchar, sites)
   })
-  beech(silver)
-  # "beech" is the plot name
-  # renderPlot
 
 })
 
