1ccaa2ca3ea5adf086259d2d3b74fa00246e87dd
Barry Polley <barryp@well.com>
2014-11-23 21:02:04 +1300

    Cleaned up but error when no checkbox selected

--- server.R
+++ server.R
@@ -3,19 +3,19 @@ silver <- read.csv("SilverBeechViable.csv", header=TRUE)
 shinyServer(function(input, output) {
   # Use the input checkboxes to determine which lines to plot
   output$beech <- renderPlot({
-    ## frame
+    ## drawframe
     plot(silver$month, silver$Cum.Sum,
          type="n",
          xlab="Month during year 2014",
          ylab="Cumulative count of viable Silver Beech seeds")
-    ## setup
+    ## set up what's going to be plotted
+    ## throws error when no checkbox is selected
     sites <- input$siteGroup
-    #sites <- c("Borer Flat", "Hunters Hill", "Rough Creek", "Station Creek", "Thisbe Stream", "Turret Head", "Wainui Stream")
     elementCount <- length(sites)
     colors <- rainbow(elementCount)
     linetype <- c(1:elementCount)
     plotchar <- seq(18, 18+elementCount, 1)
-    ## lines
+    ## draw lines
     for (i in 1:elementCount) 
     {
       loc <- subset(silver, MonitoringPlace == sites[i])
@@ -23,7 +23,7 @@ shinyServer(function(input, output) {
             type="b", lwd=2,
             lty=linetype[i],col=colors[i],pch=plotchar[i])
     }
-    ## legend
+    ## add legend
     legend("topleft", inset=.05, title="[Site]", col=colors, pch=plotchar, sites)
   })
 
--- ui.R
+++ ui.R
@@ -1,6 +1,5 @@
-silver <- read.csv("SilverBeechViable.csv", header=TRUE)
-
 shinyUI(fluidPage(
+  
   titlePanel("Seed Fall Counts for 2014"),
 
   sidebarLayout(
@@ -8,15 +7,17 @@ shinyUI(fluidPage(
       h3("How To"),
       p("Compare the seed fall for the 2014 season across several New Zealand sites by selecting and unselecting the checkboxes. A strong upward trend in any given location implies that predator control will be needed to protect native birds near that location."),
       br(),
-      p("The SeedRain slide show explains what this pattern means for conservation in more detail."),
+      p("The SeedRain slide show explains in more detail what this pattern means for conservation."),
       br(),
       checkboxGroupInput("siteGroup", 
         label = h3("Sites"), 
         choices = list("Borer Flat", 
            "Hunters Hill", "Rough Creek",
            "Station Creek", "Thisbe Stream",
-           "Turret Head", "Wainui Stream"), selected="Borer Flat")
+           "Turret Head", "Wainui Stream"),
+           selected="Borer Flat")
       ),
+    
     mainPanel(
       h2("Seed counts by month"),
       plotOutput("beech", height="600px")
